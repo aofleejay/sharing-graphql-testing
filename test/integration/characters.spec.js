19bf5d2451e6ec1288787a7135efde1d50721a43
aofleejay <aofleejay@hotmail.com>
2017-08-31 23:48:55 +0700

    checkpoint 3 : integration testing

--- test/integration/characters.spec.js
+++ test/integration/characters.spec.js
@@ -2,9 +2,67 @@ import sinon from 'sinon'
 import { expect } from 'chai'
 import supertest from 'supertest'
 import axios from 'axios'
+import server from '../../src/server'
+import { GOT_SERVICE } from '../../src/constants/endpoints'
 
 describe('Test query', () => {
-  it('has correct characters query', () => {
-    
+  let mockAxios
+
+  beforeEach(() => {
+    mockAxios = sinon.mock(axios)
+  })
+
+  afterEach(() => {
+    mockAxios.restore()
+  })
+
+  it('has correct characters query', (done) => {
+    const query = {
+      query: `
+        query {
+          characters {
+            id
+            name
+            gender {
+              name
+            }
+          }
+        }
+      `
+    }
+    const expected = {
+      characters: [
+        {
+          id: "1",
+          name: "John Snow",
+          gender: {
+            name: "Male"
+          }
+        }
+      ]
+    }
+    mockAxios.expects('get').withArgs(`${GOT_SERVICE}/characters`).once().returns(Promise.resolve({
+      data: [
+        {
+          id: "1",
+          name: "John Snow",
+          gender: {
+            name: "Male"
+          }
+        }
+      ]
+    }))
+
+    supertest.agent(server)
+      .post('/graphql')
+      .send(query)
+      .then(res => {
+        mockAxios.verify()
+        expect(res.body.data).to.deep.equals(expected)
+        done()
+      })
+      .catch(err => {
+        done(err)
+      })
   })
 })
