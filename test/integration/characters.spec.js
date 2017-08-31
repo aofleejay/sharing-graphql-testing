e3da866589f580ca3b210ef8fe35bcac30b4afdc
aofleejay <aofleejay@hotmail.com>
2017-08-31 22:14:55 +0700

    checkpoint 0 : starter workshop

--- test/integration/characters.spec.js
+++ test/integration/characters.spec.js
@@ -6,53 +6,7 @@ import server from '../../src/server'
 import { GOT_SERVICE } from '../../src/constants/endpoints'
 
 describe('Test query', () => {
-  it('has correct characters query', (done) => {
-    const request = supertest.agent(server)
-    const query = {
-      query: `
-        query {
-          characters {
-            id
-            name
-            gender {
-              name
-            }
-          }
-        }
-      `
-    }
-    const expected = {
-      characters: [
-        {
-          id: "1",
-          name: "John Snow",
-          gender: {
-            name: "Male"
-          }
-        }
-      ]
-    }
-    const serviceResponse = [
-      {
-        id: "1",
-        name: "John Snow",
-        gender: {
-          name: "Male"
-        },
-      },
-    ]
-    const mockAxios = sinon.mock(axios)
-    mockAxios.expects('get').withArgs(`${GOT_SERVICE}/characters`).once().returns(Promise.resolve({ data: serviceResponse }))
-
-    request.post('/graphql')
-    .set('Accept', 'application/json')
-    .send(query)
-    .then(res => {
-      expect(res.body.data).to.deep.equals(expected)
-      done()
-    })
-    .catch(err => {
-      done(err)
-    })
+  it('has correct characters query', () => {
+    
   })
 })
