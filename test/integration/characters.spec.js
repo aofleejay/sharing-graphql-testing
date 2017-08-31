3aed5b2f3429a9ef05b90352f99479a6a1f5bdcf
aofleejay <aofleejay@hotmail.com>
2017-08-31 22:16:32 +0700

    remove dependency

--- test/integration/characters.spec.js
+++ test/integration/characters.spec.js
@@ -2,8 +2,6 @@ import sinon from 'sinon'
 import { expect } from 'chai'
 import supertest from 'supertest'
 import axios from 'axios'
-import server from '../../src/server'
-import { GOT_SERVICE } from '../../src/constants/endpoints'
 
 describe('Test query', () => {
   it('has correct characters query', () => {
--- test/unit/characters.spec.js
+++ test/unit/characters.spec.js
@@ -1,11 +1,8 @@
-import { GraphQLString, GraphQLNonNull } from 'graphql'
 import sinon from 'sinon'
 import { expect } from 'chai'
-import rootSchema from '../../src/schemas/rootSchema'
-import * as GOTService from '../../src/services/GOTService'
 
 describe('Test schema', () => {
-  it('has correct character schema', () => {
+  it.only('has correct character schema', () => {
     
   })
 
