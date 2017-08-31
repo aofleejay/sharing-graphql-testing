90e070aad566b9c83fc22ab9f82aecdc24b03286
aofleejay <aofleejay@hotmail.com>
2017-08-31 22:48:47 +0700

    test graphql schema

--- test/unit/characters.spec.js
+++ test/unit/characters.spec.js
@@ -1,13 +1,27 @@
 import sinon from 'sinon'
 import { expect } from 'chai'
+import { GraphQLString, GraphQLNonNull } from 'graphql'
+import rootSchema from '../../src/schemas/rootSchema'
 
 describe('Test schema', () => {
-  it.only('has correct character schema', () => {
-    
+  it('has correct character schema', () => {
+    const characterSchema = rootSchema._typeMap.Character.getFields()
+
+    expect(characterSchema).to.have.property('name')
+    expect(characterSchema.name.type).to.deep.equals(GraphQLString)
+
+    expect(characterSchema).to.have.property('id')
+    expect(characterSchema.id.type).to.deep.equals(new GraphQLNonNull(GraphQLString))
+
+    expect(characterSchema).to.have.property('gender')
+    expect(characterSchema.gender.type).to.deep.equals(rootSchema._typeMap.Gender)
   })
 
   it('has correct gender schema', () => {
-    
+    const genderSchema = rootSchema._typeMap.Gender.getFields()
+
+    expect(genderSchema).to.have.property('name')
+    expect(genderSchema.name.type).to.deep.equals(GraphQLString)
   })
 })
 
